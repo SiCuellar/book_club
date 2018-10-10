16d85ace6cfa92bee2320e541c4ca00e2723de29
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-10-10 15:23:05 -0600

    fucntioning rspec added author connections

--- spec/features/user_sees_all_books_spec.rb
+++ spec/features/user_sees_all_books_spec.rb
@@ -4,13 +4,19 @@ require 'rails_helper'
 describe 'book index' do
   it 'user can see all book titles' do
 
-    book_author_1 = BookAuthor.create({book_id: 1, author_id: 1 })
 
-    author_1 = Author.create(name: "basic Ttttttom")
+
 
     book_1 = Book.create!(title: "The Count of Monte Cristo", page_count: 346, year: 1844)
+    author_1 = book_1.authors.create(name: "Alexander Dumas")
+
+
     book_2 = Book.create!(title: "The Art of War", page_count: 246, year: 1)
+    author_2 = book_2.authors.create(name: "Sun Tzue")
+
     book_3 = Book.create!(title: "Crime and Punishment", page_count: 458, year: 1866)
+    author_3 = book_3.authors.create(name: "Fyodor Dostoyevsky")
+
 
     visit 'books'
 
@@ -22,6 +28,6 @@ describe 'book index' do
     expect(page).to have_content("Page Count: #{book_3.page_count}")
     expect(page).to have_content("Publication year: #{book_3.year}")
     expect(page).to have_content("Publication year: #{book_3.year}")
-    # expect(page).to have_content("Author(s): #{book_author_1.author_id}")
+    expect(page).to have_content("Author(s): #{author_1.name}")
   end
 end
