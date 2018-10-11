71e60ecac4a1b6c5a36d90a8621ae75a1051e12c
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-10-11 10:04:49 -0600

    added further validations to model testing

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,14 @@
+# Book Class
 class Book < ApplicationRecord
-  validates_presence_of :title
+  validates_presence_of :title,
+                        :page_count,
+                        :year_published
+
   has_many :book_authors
   has_many :authors, through: :book_authors
+  has_many :reviews
+
+  def average_rating
+    reviews.average(:rating).to_f.round(1)
+  end
 end
--- spec/features/user_sees_all_books_spec.rb
+++ spec/features/user_sees_all_books_spec.rb
@@ -2,46 +2,45 @@ require 'pry'
 require 'rails_helper'
 
 describe 'book index' do
-  it 'user can see all book titles' do
+
+  before (:each) do
+    @user_1 = User.create(name: "Silver")
+    @user_2 = User.create(name: "Tom")
+
+    @book_1 = Book.create!(title: "The Count of Monte Cristo", page_count: 346, year_published: 1844)
+    @book_2  = Book.create!(title: "The Road", page_count: 349, year_published: 1999)
+    @book_3 = Book.create!(title: "Crime and Punishment", page_count: 458, year_published: 1866)
 
 
+    @author_1 = @book_1.authors.create(name: "Alexander Dumas")
+    @author_2 = @book_2.authors.create(name: "Something McCormick")
+    @author_3 = @book_3.authors.create(name: "Fyodor Dostoyevsky")
 
-
-    book_1 = Book.create!(title: "The Count of Monte Cristo", page_count: 346, year_published: 1844)
-    author_1 = book_1.authors.create(name: "Alexander Dumas")
-
-
-    book_2 = Book.create!(title: "The Art of War", page_count: 246, year_published: 1)
-    author_2 = book_2.authors.create(name: "Sun Tzue")
-
-    book_3 = Book.create!(title: "Crime and Punishment", page_count: 458, year_published: 1866)
-    author_3 = book_3.authors.create(name: "Fyodor Dostoyevsky")
-
-
-    visit 'books'
-
-    expect(page).to have_content("Title: #{book_1.title}")
-    expect(page).to have_content("Publication year: #{book_1.year_published}")
-    expect(page).to have_content("Title: #{book_2.title}")
-    expect(page).to have_content("Page Count: #{book_2.page_count}")
-    expect(page).to have_content("Page Count: #{book_3.page_count}")
-    expect(page).to have_content("Publication year: #{book_3.year_published}")
-    expect(page).to have_content("Publication year: #{book_3.year_published}")
-    expect(page).to have_content("Author(s): #{author_1.name}")
+    @review_1 = @user_1.reviews.create(title: "This Sucked", review: "There are ways.", rating: 1.5, book_id: @book_1.id)
+    @review_2 = @user_1.reviews.create(title: "Basic", review: "This was okay.", rating: 3.0, book_id: @book_1.id)
+    @review_3 = @user_2.reviews.create(title: "Awesome", review: "Best book ever.", rating: 4.5, book_id: @book_2.id)
+    @review_4 = @user_2.reviews.create(title: "Meh!", review: "I do not even know.", rating: 3.0, book_id: @book_2.id)
   end
 
-  xit 'user can see total book ratings and average' do
 
-    book_1 = Book.create!(title: "The Count of Monte Cristo", page_count: 346, year_published: 1844)
-    author_1 = book_1.authors.create(name: "Alexander Dumas")
+  it 'user can see all book information' do
+    visit '/books'
 
+    expect(page).to have_content("Title: #{@book_1.title}")
+    expect(page).to have_content("Publication year: #{@book_1.year_published}")
+    expect(page).to have_content("Title: #{@book_2.title}")
+    expect(page).to have_content("Page Count: #{@book_2.page_count}")
+    expect(page).to have_content("Page Count: #{@book_3.page_count}")
+    expect(page).to have_content("Publication year: #{@book_3.year_published}")
+    expect(page).to have_content("Publication year: #{@book_3.year_published}")
+    expect(page).to have_content("Author(s): #{@author_1.name}")
+  end
 
+  it 'user can see total book ratings and average' do
 
-    review_1 =
+    visit '/books'
 
-    visit 'books'
-
-    expect(page).to have_content("Rating: #{4.5}")
+    expect(page).to have_content("Rating: #{2.25}")
   end
 
 end
