66765d66f2bc8717447d379a3a21fcd7681847b1
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-10-14 17:58:30 -0600

    added initial testing for user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,15 +13,15 @@ class User < ApplicationRecord
 
   end
   #
-  def user_review_sort
-    select('users.*, count(reviews) as review_count')
-    .joins(:reviews)
-    .group(:user_id, :id)
-  end
-
-  def self.most_active_users
-    user_review_sort
-    order("review_count DESC")
-    .limit(3)
-  end
+#   def user_review_sort
+#     select('users.*, count(reviews) as review_count')
+#     .joins(:reviews)
+#     .group(:user_id, :id)
+#   end
+#
+#   def self.most_active_users
+#     user_review_sort
+#     order("review_count DESC")
+#     .limit(3)
+#   end
 end
